ff4bf4cdca2b0ae9e5ed24dacd8cec0d9868b3f7
Amitjindam7755 <145328964+Amitjindam7755@users.noreply.github.com>
2023-11-15 18:18:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const taskInput= document.getElementById("task");
+    const addTaskButton= doucment.getElementById("addTask");
+    const taskList= document.getElementById("taskList");
+
+    addTaskButton.addEventListener("click",function(){
+        const taskInput = taskInput.value;
+        if(taskText.trim()!=="")
+        {
+            const taskItem = document.createElement("li");
+            taskItem.innerHTML=''
+            {<span>$("taskText")</span>};
+            <button class="Delete-button">Delete</button>
+        taskList.appendChild(taskItem);
+        taskInput.value="";
+        ''};
+     });
+    taskList.addEventListener("click",function(event){""});
+    {
+        if (Event.target.classList.contains("delete-button")){
+            Event.target.parentElement.remove();
+     };
+    };
+   
+});
